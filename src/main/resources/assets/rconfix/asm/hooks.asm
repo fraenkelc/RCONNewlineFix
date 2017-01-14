5c2c59fc56cbae2eb2bcd8159ff4480d9cde8e6f
Christian Fraenkel <fraenkelc@users.noreply.github.com>
2017-01-14 02:36:01 +0100

    Use proper names for fields so the work in real environments

--- src/main/resources/assets/rconfix/asm/hooks.asm
+++ src/main/resources/assets/rconfix/asm/hooks.asm
@@ -1,10 +1,10 @@
 list addNewline
 ALOAD 0
-GETFIELD net/minecraft/network/rcon/RConConsoleSource.buffer : Ljava/lang/StringBuffer;
+GETFIELD net/minecraft/network/rcon/RConConsoleSource.field_70009_b : Ljava/lang/StringBuffer;
 INVOKEVIRTUAL java/lang/StringBuffer.length ()I
 IFLE LELSE
 ALOAD 0
-GETFIELD net/minecraft/network/rcon/RConConsoleSource.buffer : Ljava/lang/StringBuffer;
+GETFIELD net/minecraft/network/rcon/RConConsoleSource.field_70009_b : Ljava/lang/StringBuffer;
 BIPUSH 10
 INVOKEVIRTUAL java/lang/StringBuffer.append (C)Ljava/lang/StringBuffer;
 POP
